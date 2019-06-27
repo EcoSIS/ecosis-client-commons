f30c62d2a9294fbb9bdf8b61f3a020c48b3c03bb
Justin Merz <jrmerz@gmail.com>
2019-06-27 09:37:56 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// STYLES
+import litCss from "./lib/styles/lit-css"
+import "./lib/styles/shared-styles.js"
+import "./lib/styles/style-properties.js"
+import sharedStylesHtml from "./lib/styles/shared-styles.html"
+import stylePropertiesHtml from "./lib/styles/style-properties.html"
+
+export {
+  litCss, sharedStylesHtml, stylePropertiesHtml
+}
