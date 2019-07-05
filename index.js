b0313dcd4d68fd0d1dbb4f0e028eba6904d01e42
Justin Merz <jrmerz@gmail.com>
2019-07-05 08:32:40 -0700

    adding common search header

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import "./lib/styles/shared-styles.js"
 import "./lib/styles/style-properties.js"
 import sharedStylesHtml from "./lib/styles/shared-styles.html"
 import stylePropertiesHtml from "./lib/styles/style-properties.html"
+import './lib/elements/search-header/app-search-header'
 
 export {
   litCss, sharedStylesHtml, stylePropertiesHtml
