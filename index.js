9d2a627e58da9628c348ef4e1b5a7bfc19a52d9e
Justin Merz <jrmerz@gmail.com>
2019-08-04 11:05:44 -0700

    adding filters

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import sharedStylesHtml from "./lib/styles/shared-styles.html"
 import stylePropertiesHtml from "./lib/styles/style-properties.html"
 import './lib/elements/search-header/app-search-header'
 import './lib/elements/search-pagination/app-search-pagination'
+import './lib/elements/search-filters/app-filter-panel'
+import './lib/elements/utils/app-popup'
 
 export {
   litCss, sharedStylesHtml, stylePropertiesHtml
